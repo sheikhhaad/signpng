52ca2c6268536cde8cb36ba2f1789de6b375c2c2
sheikhhaad <sheikhhaad6@gmail.com>
2026-04-16 22:13:31 +0500

    .

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,39 +1,34 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
-  <url>
-    <loc>https://signpng.vercel.app/</loc>
-    <lastmod>2025-09-26</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
-
-  <url>
-    <loc>https://signpng.vercel.app/signature-to-png</loc>
-    <lastmod>2025-09-26</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-
-  <url>
-    <loc>https://signpng.vercel.app/blog</loc>
-    <lastmod>2025-09-26</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.9</priority>
-  </url>
-
-  <url>
-    <loc>https://signpng.vercel.app/contact</loc>
-    <lastmod>2025-09-26</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-
-  <url>
-    <loc>https://signpng.vercel.app/privacy</loc>
-    <lastmod>2025-09-26</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  
-</urlset>
+export default function sitemap() {
+  return [
+    {
+      url: "https://signpng.vercel.app/",
+      lastModified: "2025-09-26",
+      changeFrequency: "weekly",
+      priority: 1.0,
+    },
+    {
+      url: "https://signpng.vercel.app/signature-to-png",
+      lastModified: "2025-09-26",
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://signpng.vercel.app/blog",
+      lastModified: "2025-09-26",
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: "https://signpng.vercel.app/contact",
+      lastModified: "2025-09-26",
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: "https://signpng.vercel.app/privacy",
+      lastModified: "2025-09-26",
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+  ];
+}
